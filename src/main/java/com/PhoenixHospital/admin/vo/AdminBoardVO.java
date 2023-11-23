91ec9781947ccecb5c6f0876b63d3da5a627b287
JS.YJY <dbwldud6778@daum.net>
2023-11-23 10:31:01 +0900

    231122 adminVO

---
+++ src/main/java/com/PhoenixHospital/admin/vo/AdminBoardVO.java
@@ -0,0 +1,103 @@
+package com.PhoenixHospital.admin.vo;
+
+public class AdminBoardVO {
+    private int boNo;
+    private String boTitle;
+    private String categoryCode;
+    private String boContents;
+    private String boWriter;
+    private int boHit;
+    private String boRegDat;
+    private String boModDate;
+    private String b0DelYn;
+    private String atchNo;
+    private String boPass;
+
+    public int getBoNo() {
+        return boNo;
+    }
+
+    public void setBoNo(int boNo) {
+        this.boNo = boNo;
+    }
+
+    public String getBoTitle() {
+        return boTitle;
+    }
+
+    public void setBoTitle(String boTitle) {
+        this.boTitle = boTitle;
+    }
+
+    public String getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(String categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getBoContents() {
+        return boContents;
+    }
+
+    public void setBoContents(String boContents) {
+        this.boContents = boContents;
+    }
+
+    public String getBoWriter() {
+        return boWriter;
+    }
+
+    public void setBoWriter(String boWriter) {
+        this.boWriter = boWriter;
+    }
+
+    public int getBoHit() {
+        return boHit;
+    }
+
+    public void setBoHit(int boHit) {
+        this.boHit = boHit;
+    }
+
+    public String getBoRegDat() {
+        return boRegDat;
+    }
+
+    public void setBoRegDat(String boRegDat) {
+        this.boRegDat = boRegDat;
+    }
+
+    public String getBoModDate() {
+        return boModDate;
+    }
+
+    public void setBoModDate(String boModDate) {
+        this.boModDate = boModDate;
+    }
+
+    public String getB0DelYn() {
+        return b0DelYn;
+    }
+
+    public void setB0DelYn(String b0DelYn) {
+        this.b0DelYn = b0DelYn;
+    }
+
+    public String getAtchNo() {
+        return atchNo;
+    }
+
+    public void setAtchNo(String atchNo) {
+        this.atchNo = atchNo;
+    }
+
+    public String getBoPass() {
+        return boPass;
+    }
+
+    public void setBoPass(String boPass) {
+        this.boPass = boPass;
+    }
+}
